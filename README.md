8e339ce4bdf543b1a97b7e899c424d6bb245cc58
Siyam <w.siyam10@gmail.com>
2026-06-23 12:15:51 +0600

    Initial commit - Added SRP practice

---
+++ README.md
@@ -0,0 +1,19 @@
+# SOLID Principles Practice
+
+This repository contains my practice implementations of the SOLID design principles in C#.
+
+## Principles
+
+- SRP (Single Responsibility Principle)
+- OCP (Open Closed Principle)
+- LSP (Liskov Substitution Principle)
+- ISP (Interface Segregation Principle)
+- DIP (Dependency Inversion Principle)
+
+## Language
+
+- C#
+
+## Purpose
+
+This repository is for learning and practicing software design principles.
---
+++ Single-Responsibility-Principle/SRP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class SRP
+    {
+        public class User
+        {
+            public string Name { get; set; }
+        }
+
+        public class UserRepo
+        {
+            public void Save(User user)
+            {
+                Console.WriteLine($"{user.Name} saved");
+            }
+        }
+
+        public class EmainService
+        {
+            public void SendWelcomeEmail(User user)
+            {
+                Console.WriteLine($"Email sent to {user.Name}");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            User man = new User();
+            man.Name = "siyam";
+            UserRepo repo = new UserRepo();
+            repo.Save(man);
+            EmainService email = new EmainService();
+            email.SendWelcomeEmail(man);
+        }
+    }
+}
