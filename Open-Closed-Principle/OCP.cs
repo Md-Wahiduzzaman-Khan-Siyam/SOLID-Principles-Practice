7c82405bed7d16f099b3a083ad465412337d9217
WK Siyam <94302949+Md-Wahiduzzaman-Khan-Siyam@users.noreply.github.com>
2026-06-28 21:17:48 +0600

    Add files via upload

---
+++ Open-Closed-Principle/OCP.cs
@@ -0,0 +1,54 @@
+﻿// software entities (classec, modules, functions) should be open for extension,
+// but closed for modification
+
+using System;
+
+namespace ConsoleApp1
+{
+    class OCP
+    {
+        public interface IPaymentMethod
+        {
+            void Pay();
+        }
+        public class BkashPayment : IPaymentMethod
+        {
+            public void Pay()
+            {
+                Console.WriteLine("Bkash Payment Successful");
+            }
+        }
+        public class NagadPayment : IPaymentMethod
+        {
+            public void Pay()
+            {
+                Console.WriteLine("Nagad Payment Successfll");
+            }
+        }
+
+        public class CardPayment : IPaymentMethod
+        {
+            public void Pay()
+            {
+                Console.WriteLine("Card Payment Successful");
+            }
+        }
+
+        // business logic class
+        public class PaymentProcessor()
+        {
+            public void ProcessPayment(IPaymentMethod paymentMethod)
+            {
+                paymentMethod.Pay();
+            }
+        }
+        
+        public static void Main()
+        {
+            PaymentProcessor payment = new PaymentProcessor();
+            payment.ProcessPayment(new BkashPayment());
+            payment.ProcessPayment(new NagadPayment());
+            payment.ProcessPayment(new CardPayment());
+        }
+    }
+}
