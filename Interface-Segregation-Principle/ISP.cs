c35395e19f75024ea9a5a04f9c342a8fffa9c65c
WK Siyam <94302949+Md-Wahiduzzaman-Khan-Siyam@users.noreply.github.com>
2026-06-29 03:17:10 +0600

    Add files via upload

---
+++ Interface-Segregation-Principle/ISP.cs
@@ -0,0 +1,58 @@
+﻿// No client should be forced to depend on methods it does not use
+
+using System;
+
+class ISP
+{
+    public interface IBird
+    {
+        void Eat();
+    }
+
+    public interface IFlyable : IBird
+    {
+        void Fly();
+    }
+
+    public interface ISwimmable : IBird
+    {
+        void Swim();
+    }
+
+    public class Parrot : IFlyable
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Parrot Eat");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Parrot Fly");
+        }
+    }
+
+    public class Penguin : ISwimmable
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Penguin Eat");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Penguin Swim");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFlyable flyingBird = new Parrot();
+            flyingBird.Fly();
+            ISwimmable swimmingBird = new Penguin();
+            swimmingBird.Swim();
+        }
+    }    
+}
