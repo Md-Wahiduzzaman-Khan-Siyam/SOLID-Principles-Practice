6ed493871df240cc833a058ba181c5e3fb7ae4c0
WK Siyam <94302949+Md-Wahiduzzaman-Khan-Siyam@users.noreply.github.com>
2026-06-29 02:18:51 +0600

    Add files via upload

---
+++ Liskov-Substitution-Principle/LSP.cs
@@ -0,0 +1,59 @@
+﻿// Object of a superclass should be replaceable with object of a subclass
+// without breaking the application 
+
+using System;
+
+class LSP
+{
+    public interface IBird
+    {
+        void Eat();
+    }
+
+    public interface IFlyable : IBird
+    {
+        void Fly();
+    }
+
+    public interface ISwimmable : IBird
+    {
+        void Swim();
+    }
+
+    public class Parrot : IBird, IFlyable
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Parrot Eat");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Parrot Fly");
+        }
+    }
+
+    public class Penguin : IBird, ISwimmable
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Penguin Eat");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Penguin Swim");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBird parrot = new Parrot();
+            parrot.Eat();
+            Penguin penguin = new Penguin();
+            penguin.Swim();
+        }
+    }    
+}
