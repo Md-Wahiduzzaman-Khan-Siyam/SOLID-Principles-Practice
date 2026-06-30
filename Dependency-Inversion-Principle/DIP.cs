3c62054effc1485ef6c996d03529e743132d7626
WK Siyam <94302949+Md-Wahiduzzaman-Khan-Siyam@users.noreply.github.com>
2026-06-30 15:16:03 +0600

    Add files via upload

---
+++ Dependency-Inversion-Principle/DIP.cs
@@ -0,0 +1,53 @@
+﻿// High level module should not depend on low level module
+
+using System;
+
+class DIP
+{
+    public interface IMessageService
+    {
+        void send();
+    }
+    
+    // concrete class or low level module
+    public class EmailService : IMessageService
+    {
+        public void send()
+        {
+            Console.WriteLine("Email sent");
+        }
+    }
+    public class SmsService : IMessageService
+    {
+        public void send()
+        {
+            Console.WriteLine("Sms sent");
+        }
+    }
+    
+    // business logic class or high level module
+    public class NotificationService
+    {
+        IMessageService _messageService;
+
+        public NotificationService(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+        public void Notify()
+        {
+            _messageService.send();
+        }
+    }
+
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            EmailService emailService = new EmailService();
+            SmsService smsService = new SmsService();
+            emailService.send();
+            smsService.send();
+        }
+    }
+}
